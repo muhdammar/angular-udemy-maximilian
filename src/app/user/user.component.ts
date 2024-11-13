66eeead61b636a9a407411baad0331b6c062de6c
Muhammad Ammar <ammar.aziz.work@gmail.com>
2024-11-13 23:50:03 +0800

    change code

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,25 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, computed } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
-const randomIndex = Math.floor(Math.random()* DUMMY_USERS.length)
+// const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 @Component({
   selector: 'app-user',
   standalone: true,
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
-  //we can add private selected to make it private 
+  /**
+   * COMMENT THIS SINCE NO LONGER NEED FOR TUTORIAL 30 ONWARDS
+   */
+  //we can add private selected to make it private
   //so it not available in the template but not in this case
-  selectedUser = DUMMY_USERS[randomIndex];
+
+  // selectedUser = DUMMY_USERS[randomIndex];
 
   //computed
-  get imagePath(){
-    return 'assets/users/'+ this.selectedUser.avatar
-  }
+  // get imagePath(){
+  //   return 'assets/users/'+ this.selectedUser.avatar
+  // }
+  //another way computed
+  // imagePathComputed = computed(() => 'assets/users/' +this.selectedUser.avatar)
 
   //events
-  onSelectUser(){
-    const randomIndex = Math.floor(Math.random()* DUMMY_USERS.length)
-    this.selectedUser = DUMMY_USERS[randomIndex];
+  onSelectUser() {
+    /**
+     * COMMENT THIS SINCE NO LONGER NEED FOR TUTORIAL 30 ONWARDS
+     */
+    // const randomIndex = Math.floor(Math.random()* DUMMY_USERS.length)
+    // this.selectedUser = DUMMY_USERS[randomIndex];
   }
 }

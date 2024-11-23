500650d2a4cb76e1ad0f88e6e6035b814b914ec3
Muhammad Ammar <ammar.aziz.work@gmail.com>
2024-11-23 14:48:32 +0800

    @Output vs output

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, input, computed, Output, EventEmitter } from '@angular/core';
+import { Component, Input, input, computed, Output, output, EventEmitter } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 // const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 @Component({
@@ -8,15 +8,21 @@ import { DUMMY_USERS } from '../dummy-users';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-
   //this is @Input decorator
   //"avatar!" <--- this symbol was typescript features to allow the value is set outside this file
-  @Input({required: true}) avatar!: String;
-  @Input({required: true}) name!: String;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+  @Input({ required: true }) id!: string;
 
-  //@Output properties
+  //@Output properties, old approach, most project use this
   @Output() select = new EventEmitter();
-   /**
+
+  //another new sytax for output properties,
+  // it same like above but more shortcut
+  // select = output<string>();
+
+
+  /**
    * COMMENT THIS SINCE NO LONGER NEED to use signal, use old approach
    */
   //this input() is from signal features
@@ -29,7 +35,7 @@ export class UserComponent {
   //avatar = input.required<string>(); //input is required with generic type
 
   //these are READ ONLY signal
-  //we cant change the avatar from signal function 
+  //we cant change the avatar from signal function
   //like this.avatar.set("new image");
   // avatar = input<string>();
   // name = input<string>();
@@ -41,14 +47,12 @@ export class UserComponent {
   // selectedUser = DUMMY_USERS[randomIndex];
 
   //computed
-  get imagePath(){
-    return 'assets/users/'+ this.avatar
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
   }
 
   //another way computed for signal
   // imagePathComputed = computed(() => 'assets/users/' + this.avatar())
-
-
 
   //events
   onSelectUser() {
@@ -57,5 +61,6 @@ export class UserComponent {
      */
     // const randomIndex = Math.floor(Math.random()* DUMMY_USERS.length)
     // this.selectedUser = DUMMY_USERS[randomIndex];
+    this.select.emit(this.id);
   }
 }

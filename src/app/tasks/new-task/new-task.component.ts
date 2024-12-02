cff7959c72272cb09f29d64be41bb2b22f71c2a8
Muhammad Ammar <ammar.aziz.work@gmail.com>
2024-12-02 15:17:59 +0800

    add two ways binding in angular

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,14 +1,24 @@
-import { Component, Output, EventEmitter } from '@angular/core';
-
+import { Component, Output, EventEmitter, signal } from '@angular/core';
+//form module is collection for directive and features
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-new-task',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css',
 })
 export class NewTaskComponent {
   @Output() cancel = new EventEmitter<void>();
+  enteredTitle=''
+  enteredSummary=''
+  enteredDate=''
+
+  //if want to use signal
+  // enteredTitle=signal('')
+  // enteredSummary=signal('')
+  // enteredDate=signal('')
+
   onCancel() {
     this.cancel.emit();
   }

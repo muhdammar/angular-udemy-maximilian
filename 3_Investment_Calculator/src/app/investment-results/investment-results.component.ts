2ba19038285530d9a898077fb8b96aa0d295af31
Muhammad Ammar <ammar.aziz.work@gmail.com>
2025-03-09 15:22:34 +0800

    add currency pipe

--- 3_Investment_Calculator/src/app/investment-results/investment-results.component.ts
+++ 3_Investment_Calculator/src/app/investment-results/investment-results.component.ts
@@ -1,10 +1,10 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule, CurrencyPipe } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-investment-results',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CurrencyPipe],
   templateUrl: './investment-results.component.html',
   styleUrl: './investment-results.component.css'
 })

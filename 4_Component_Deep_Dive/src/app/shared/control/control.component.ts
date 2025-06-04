b4bbd01d7c73e67e35b4885a07fb1945abd55173
Ammar Abdul Aziz <muhammad.ammar@ezyhaul.com>
2025-06-04 10:56:41 +0800

    #112 View Encapsulation configuration

--- 4_Component_Deep_Dive/src/app/shared/control/control.component.ts
+++ 4_Component_Deep_Dive/src/app/shared/control/control.component.ts
@@ -1,11 +1,12 @@
-import { Component, input } from '@angular/core';
+import { Component, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
   standalone: true,
   imports: [],
   templateUrl: './control.component.html',
-  styleUrl: './control.component.css'
+  styleUrl: './control.component.css',
+  encapsulation: ViewEncapsulation.None, // Use None to avoid styles being scoped to this component (global styles)
 })
 export class ControlComponent {
   label = input.required<string>();

d629b14d1935f393657845fb019760a6ca2393c8
Ammar Abdul Aziz <muhammad.ammar@ezyhaul.com>
2025-06-04 11:48:26 +0800

    114 use host selector for styles

--- 4_Component_Deep_Dive/src/app/shared/control/control.component.ts
+++ 4_Component_Deep_Dive/src/app/shared/control/control.component.ts
@@ -7,6 +7,9 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
   templateUrl: './control.component.html',
   styleUrl: './control.component.css',
   encapsulation: ViewEncapsulation.None, // Use None to avoid styles being scoped to this component (global styles)
+  host: {
+    class: 'control', // Add a class to the host element for styling
+  }
 })
 export class ControlComponent {
   label = input.required<string>();

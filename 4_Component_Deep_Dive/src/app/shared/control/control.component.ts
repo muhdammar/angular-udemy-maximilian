1918c589bdc095979491daadeb49dffacce0af2f
Ammar Abdul Aziz <muhammad.ammar@ezyhaul.com>
2025-06-04 12:09:59 +0800

    116 HostBinding & HostElement

--- 4_Component_Deep_Dive/src/app/shared/control/control.component.ts
+++ 4_Component_Deep_Dive/src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, ViewEncapsulation } from '@angular/core';
+import { Component, Host, HostBinding, HostListener, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -8,9 +8,21 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
   styleUrl: './control.component.css',
   encapsulation: ViewEncapsulation.None, // Use None to avoid styles being scoped to this component (global styles)
   host: {
-    class: 'control', // Add a class to the host element for styling
+    class: 'control from-host-decorator', // Add a class to the host element for styling
+    '(click)': 'onClick()' // Recommend by angular team -> Listen to onclick events on the host element
   }
 })
 export class ControlComponent {
+  //if we have a class that is used in the template, we can use HostBinding to add it to the host element
+  @HostBinding('class') hostClass = 'control from-hostbinding-decorator'; // alternative way to add a class to the host
+
+  // @HostListener('click')
+  onClick() {
+    // This method will be called when the host element is clicked
+    console.log('Control clicked');
+    // You can perform any action here, like updating a property or emitting an event
+  }
+
+
   label = input.required<string>();
 }

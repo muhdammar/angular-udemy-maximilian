5614eed11483f86e4a78e2fe2bfd2fd9d49e0eb4
muhdammar <ammar.aziz.work@gmail.com>
2025-06-05 16:23:34 +0800

    component cleanup using OnDestroy & DestroyRef

--- 4_Component_Deep_Dive/src/app/dashboard/server-status/server-status.component.ts
+++ 4_Component_Deep_Dive/src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, DestroyRef, inject, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,11 +7,13 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css',
 })
-export class ServerStatusComponent implements OnInit {
+export class ServerStatusComponent implements OnInit, OnDestroy {
+  
   currentStatus: 'online' | 'offline' | 'unknown' = 'offline';
- 
+  private interval?: ReturnType<typeof setInterval>;
+  private destroyRef = inject(DestroyRef); //angular v16+ way to handle component destruction
   ngOnInit() {
-    setInterval(() => {
+    this.interval = setInterval(() => {
       const rnd = Math.random();
       if (rnd > 0.5) {
         this.currentStatus = 'online';
@@ -22,5 +24,26 @@ export class ServerStatusComponent implements OnInit {
         this.currentStatus = 'offline'; // Simulating a server that is offline
       }
     }, 5000);
+
+  // Alternative way to handle component destruction in Angular v16+
+    const intervalDestroyRef = setInterval(() => {
+      const rnd = Math.random();
+      if (rnd > 0.5) {
+        this.currentStatus = 'online';
+      } else if (rnd > 0.2) { // Simulating a server that is online
+        this.currentStatus = 'offline'; // Simulating a server that is under maintenance
+      }
+      else {
+        this.currentStatus = 'offline'; // Simulating a server that is offline
+      }
+    }, 5000);
+    this.destroyRef.onDestroy(() => {
+      clearInterval(intervalDestroyRef);
+    });
   } 
+  ngOnDestroy(): void {
+    // Clear the interval to prevent memory leaks
+    clearTimeout(this.interval);
+  }
+
 }

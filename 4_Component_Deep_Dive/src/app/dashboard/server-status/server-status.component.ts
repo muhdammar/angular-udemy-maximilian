47f1cf411e2cceb5831c3e4bbb4aa4737880312f
muhdammar <ammar.aziz.work@gmail.com>
2025-06-06 18:47:02 +0800

    138: add signal effect

--- 4_Component_Deep_Dive/src/app/dashboard/server-status/server-status.component.ts
+++ 4_Component_Deep_Dive/src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component, DestroyRef, inject, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, DestroyRef, effect, inject, OnDestroy, OnInit, signal, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -9,37 +9,44 @@ import { Component, DestroyRef, inject, OnDestroy, OnInit, ViewEncapsulation } f
 })
 export class ServerStatusComponent implements OnInit, OnDestroy {
   
-  currentStatus: 'online' | 'offline' | 'unknown' = 'offline';
+  constructor() { 
+    effect(() => {
+      // This effect will run whenever currentStatus changes
+      console.log(`Server status changed to: ${this.currentStatus()}`);
+    });
+  }
+  currentStatus = signal<'online' | 'offline' | 'unknown'>("offline"); // Using signal to track the current status
+  
   private interval?: ReturnType<typeof setInterval>;
-  private destroyRef = inject(DestroyRef); //angular v16+ way to handle component destruction
+  // private destroyRef = inject(DestroyRef); //angular v16+ way to handle component destruction
   ngOnInit() {
     this.interval = setInterval(() => {
       const rnd = Math.random();
       if (rnd > 0.5) {
-        this.currentStatus = 'online';
+        this.currentStatus.set('online');
       } else if (rnd > 0.2) { // Simulating a server that is online
-        this.currentStatus = 'offline'; // Simulating a server that is under maintenance
+        this.currentStatus.set('offline') // Simulating a server that is under maintenance
       }
       else {
-        this.currentStatus = 'offline'; // Simulating a server that is offline
+        this.currentStatus.set('unknown'); // Simulating a server that is offline
       }
     }, 5000);
 
   // Alternative way to handle component destruction in Angular v16+
-    const intervalDestroyRef = setInterval(() => {
-      const rnd = Math.random();
-      if (rnd > 0.5) {
-        this.currentStatus = 'online';
-      } else if (rnd > 0.2) { // Simulating a server that is online
-        this.currentStatus = 'offline'; // Simulating a server that is under maintenance
-      }
-      else {
-        this.currentStatus = 'offline'; // Simulating a server that is offline
-      }
-    }, 5000);
-    this.destroyRef.onDestroy(() => {
-      clearInterval(intervalDestroyRef);
-    });
+    // const intervalDestroyRef = setInterval(() => {
+    //   const rnd = Math.random();
+    //   if (rnd > 0.5) {
+    //     this.currentStatus.set('online');
+    //   } else if (rnd > 0.2) { // Simulating a server that is online
+    //     this.currentStatus.set('offline'); // Simulating a server that is under maintenance
+    //   }
+    //   else {
+    //     this.currentStatus.set('unknown'); // Simulating a server that is offline
+    //   }
+    // }, 5000);
+    // this.destroyRef.onDestroy(() => {
+    //   clearInterval(intervalDestroyRef);
+    // });
   } 
   ngOnDestroy(): void {
     // Clear the interval to prevent memory leaks

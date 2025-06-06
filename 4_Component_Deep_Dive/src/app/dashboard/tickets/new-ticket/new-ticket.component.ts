d58ed34e938aadcaaf491a3379e420878ae0e30c
muhdammar <ammar.aziz.work@gmail.com>
2025-06-06 14:31:05 +0800

    #132 Using The viewChild Signal Function

--- 4_Component_Deep_Dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ 4_Component_Deep_Dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component,ElementRef,ViewChild } from '@angular/core';
+import { Component,ElementRef,viewChild,ViewChild } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from "../../../shared/control/control.component";
 import { FormsModule } from '@angular/forms';
@@ -12,12 +12,17 @@ import { FormsModule } from '@angular/forms';
 })
 export class NewTicketComponent {
   //@ViewChild decorator
-  @ViewChild('form') form?: ElementRef<HTMLFormElement>; 
-  // @ViewChild(ButtonComponent) btn: ButtonComponent; // This is an alternative way to access the button component if needed
+  //@ViewChild('form') form?: ElementRef<HTMLFormElement>; 
+  //@ViewChild(ButtonComponent) btn: ButtonComponent; // This is an alternative way to access the button component if needed
+
+  private form = viewChild.required<ElementRef<HTMLFormElement>>('form'); // angular 17+ syntax for required view child (signal way)
   onSubmit(titleElement: HTMLInputElement) {
     console.log('Title:', titleElement.value);
     // Handle form submission logic here
     console.log('New ticket submitted');
-    this.form?.nativeElement.reset();
+    // this.form?.nativeElement.reset();
+
+    //signal way to access the form element
+    this.form().nativeElement.reset();
   }
 }

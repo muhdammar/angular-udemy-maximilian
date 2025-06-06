b9d4f0e0dc960e30c1a1c83849d6e57d2952295b
muhdammar <ammar.aziz.work@gmail.com>
2025-06-06 10:51:37 +0800

    accessing template via @ViewChild Decorator

--- 4_Component_Deep_Dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ 4_Component_Deep_Dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,ElementRef,ViewChild } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from "../../../shared/control/control.component";
 import { FormsModule } from '@angular/forms';
@@ -11,9 +11,13 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
+  //@ViewChild decorator
+  @ViewChild('form') form?: ElementRef<HTMLFormElement>; 
+  // @ViewChild(ButtonComponent) btn: ButtonComponent; // This is an alternative way to access the button component if needed
   onSubmit(titleElement: HTMLInputElement) {
     console.log('Title:', titleElement.value);
     // Handle form submission logic here
     console.log('New ticket submitted');
+    this.form?.nativeElement.reset();
   }
 }

0c9b5a936b6733a8648c90ac07711b388e8e5a4f
muhdammar <ammar.aziz.work@gmail.com>
2025-08-20 11:25:11 +0800

    revert back for original example

--- 8_Understanding_Services_&_Dependency_Injection_Deep_Dive/src/app/tasks/tasks.component.ts
+++ 8_Understanding_Services_&_Dependency_Injection_Deep_Dive/src/app/tasks/tasks.component.ts
@@ -9,7 +9,7 @@ import { TasksService } from './tasks.service';
   standalone: true,
   templateUrl: './tasks.component.html',
   imports: [NewTaskComponent, TasksListComponent],
-  providers: [TasksService] //(ElementInjector) Registering the TasksService as a provider in this component
+  // providers: [TasksService] //(ElementInjector) Registering the TasksService as a provider in this component
                             // This makes the TasksService available only within this component and its children.                       
                             // Isolated from the root injector, so it won't be available in other parts of the application.
                             // This is useful for creating component-specific services that do not need to be shared globally.
